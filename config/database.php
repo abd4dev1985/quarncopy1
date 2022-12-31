24b101c57b1d80aa92e3ebb6b11e216e95bf9ddf
abd4dev1985 <abdyia@yahoo.com>
2023-01-01 00:15:01 +0200

    fix4

--- config/database.php
+++ config/database.php
@@ -66,7 +66,6 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT_', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
